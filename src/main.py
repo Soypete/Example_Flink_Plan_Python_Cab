fb2980da6d8e0b20fdcc3e11a10da0011a30f45b
Soypete <miriah.peterson@getweave.com>
2019-02-06 14:14:11 -0700

    clean up cpde

--- src/main.py
+++ src/main.py
@@ -1,6 +1,4 @@
-from org.apache.flink.streaming.api.functions.source import SourceFunction
-from org.apache.flink.api.common.functions import MapFunction, ReduceFunction, FilterFunction 
-from org.apache.flink.api.java.functions import KeySelector
+from org.apache.flink.api.common.functions import MapFunction, FilterFunction 
 from org.apache.flink.streaming.api.windowing.time.Time import milliseconds
 
 # find destination count!
@@ -17,18 +15,6 @@ class CountDest(MapFunction):
         count2, word2 = input2
         return (count1 + count2, word1)
     
-# # calculate the average number of passengers
-
-# class GroupBy(GroupbyFunction):
-#     def group_by(self, input):
-#         return (input[6])
-
-# class AvePass(ReduceFunction):
-#     def reduce(self, input1, input2):
-#         count1, word1 = input1
-#         count2, word2 = input2
-#         return (count1 + count2, word1)
-
 # run by flink python execution layer
 def main(factory):
     # create flink environment. This environment gives you access to the flink pre-defined operators. 
@@ -36,22 +22,9 @@ def main(factory):
     # read in cab text file. This creates a data stream.
     # Data format
     # cab_id|cab_type|cab_number_plate|cab_driver_name|ongoing_trip/not|pickup_location|destination|passenger_count
-    text = env.read_text_file("file:///cab-flink.txt") \
+    env.read_text_file("file:///cab-flink.txt") \
         .filter(FilterDestination()) \
         .map(CountDest()) \
-        .write_as_text("file:///parsed_destinations.txt")
-        # .time_window(milliseconds(50)) \
-    # text.print()
+        .output() 
 
-    # text1 = env.read_text_file("file:///cab-flink.txt")         
-    #     .group_by(GroupBy()) \
-    #     .reduce(AvePass())
-    #     .time_window(milliseconds(50)) \
-    # text1.write_text_file("file:///parsed_passengers.txt")
-
-    # text2 = env.read_text_file("file:///cab-flink.txt")         
-    #     .reduce(Sum()) \
-    #     .output()
-    #     .time_window(milliseconds(50)) \
-    # text2.write_text_file("file:///parsed_trips.txt")
     env.execute()
