4d9f7f0ba2835fbf9cebfc3c3907b9aafd2e1b45
Soypete <miriah.peterson@getweave.com>
2019-02-06 12:44:43 -0700

    step one

--- src/main.py
+++ src/main.py
@@ -3,12 +3,6 @@ from org.apache.flink.api.common.functions import FlatMapFunction, ReduceFunctio
 from org.apache.flink.api.java.functions import KeySelector
 from org.apache.flink.streaming.api.windowing.time.Time import milliseconds
 
-
-
-
-class Reduce(ReduceFunction):
-    def reduce(self):
-
 # find destination count!
         
 # get destination 
@@ -23,38 +17,41 @@ class CountDest(MapFunction):
         count2, word2 = input2
         return (count1 + count2, word1)
     
-# calculate the average number of passengers
+# # calculate the average number of passengers
 
-class FilterPassengers(FilterFunction):
-    def filterDestination(self, input):
-        return input[7]
+# class GroupBy(GroupbyFunction):
+#     def group_by(self, input):
+#         return (input[6])
 
-
-class GroupBy(GroupbyFunction):
-    def groupBy:
-
-class MaxBy(MaxbyFunction):
-    def maxby:
-
-
-class Sum(ReduceFunction):
-    def reduce(self, input1, input2):
-        count1, word1 = input1
-        count2, word2 = input2
-        return (count1 + count2, word1)
+# class AvePass(ReduceFunction):
+#     def reduce(self, input1, input2):
+#         count1, word1 = input1
+#         count2, word2 = input2
+#         return (count1 + count2, word1)
 
 # run by flink python execution layer
 def main(factory):
     # create flink environment. This environment gives you access to the flink pre-defined operators. 
     env = factory.get_execution_environment()
     # read in cab text file. This creates a data stream.
-    # Dataformat
+    # Data format
     # cab_id|cab_type|cab_number_plate|cab_driver_name|ongoing_trip/not|pickup_location|destination|passenger_count
     text = env.read_text_file("file:///cab-flink.txt") \
-        .flat_map(Tokenizer()) \
-        .key_by(Selector()) \
+        .filer(FilterDestination()) \
+        .map(CountDest()) \
         .time_window(milliseconds(50)) \
-        .reduce(Sum()) \
-        .output()
+        .pritn()\
+    # text.write_text_file("file:///parsed_destinations.txt")
+
+    # text1 = env.read_text_file("file:///cab-flink.txt")         
+    #     .group_by(GroupBy()) \
+    #     .reduce(AvePass())
+    #     .time_window(milliseconds(50)) \
+    # text1.write_text_file("file:///parsed_passengers.txt")
+
+    # text2 = env.read_text_file("file:///cab-flink.txt")         
+    #     .reduce(Sum()) \
+    #     .output()
+    #     .time_window(milliseconds(50)) \
+    # text2.write_text_file("file:///parsed_trips.txt")
     env.execute()
-    text.write_text_file("file:///parsed.txt")
