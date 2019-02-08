8aec12bd6eb0c0a97b42533391c163abf4e06b1d
Soypete <miriah.peterson@getweave.com>
2019-02-07 23:31:33 -0700

    runs, but is not selecting destination exlusively

--- src/main.py
+++ src/main.py
@@ -1,18 +1,22 @@
-from org.apache.flink.api.common.functions import FlatMapFunction, ReduceFunction
+from org.apache.flink.api.common.functions import FlatMapFunction, ReduceFunction, FilterFunction
 from org.apache.flink.api.java.functions import KeySelector
 from org.apache.flink.streaming.api.windowing.time.Time import milliseconds
 
 # find destination count!
+# select destination column
+class SelectDestination(FilterFunction):
+    def filter(self, input):
+        return input[7]
 
 # created the tuple round the word for counting
 class Tupler(FlatMapFunction):
     def flatMap(self, value, collector):
         collector.collect((1, value))
 
-# select destination column
-class SelectDestination(KeySelector):
+class Selector(KeySelector):
     def getKey(self, input):
-        return input[7]
+        return input[1]
+
 # count occurances by increasing tuple number
 class CountDest(ReduceFunction):
     def map(self, input1, input2):
@@ -29,11 +33,13 @@ def main(flink):
     # cab_id|cab_type|cab_number_plate|cab_driver_name|ongoing_trip/not|pickup_location|destination|passenger_count
     text = env.read_text_file("file:///Users/miriah.peterson@getweave.com/code/flink_tutorial/assignment_1/cab-flink.txt") # likes full source path
 
-    text.flat_map(Tupler()) \
-        .key_by(SelectDestination()) \
-        .time_window(milliseconds(50)) \
-        .reduce(CountDest()) \
-        .output()
+    text.filter(SelectDestination()) \
+        .flat_map(Tupler()) \
+        .write_as_text("file:///Users/miriah.peterson@getweave.com/code/flink_tutorial/assignment_1/cab-out.txt")
+        # .key_by(Selector()) \
+        # .time_window(milliseconds(50)) \
+        # .reduce(CountDest()) \
+        # .output()
 
     result = env.execute("cab example")
     print(result)
